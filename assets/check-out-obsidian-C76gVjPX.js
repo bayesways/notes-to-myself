428906244f4f475730a8768c1bdc2be67e9c9e51
bayesways <vabour@gmail.com>
2025-12-05 21:39:04 -0500

    Updates

---
+++ assets/check-out-obsidian-C76gVjPX.js
@@ -0,0 +1,9 @@
+const e=`---
+title: "Check out Obsidian"
+date: "2025-12-08"
+---
+
+I've been using Obsidian lately and it's great.
+
+Read my other post about it: [Obsessed with Obsidian](obsessed-with-obsidian)
+`;export{e as default};
